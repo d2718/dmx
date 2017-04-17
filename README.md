1a439b5400189a48c18c2e8f14f502d06444b81d
Ahura Mazda <pwnz0r3d@gmail.com>
2017-04-17 13:55:46 -0400

    created repo; began work

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # dmx
 A Go package for interacting with `dmenu` and some utilities that use it.
+
+### History
+
+This began many years ago when I was running
+[`wmii`](https://code.google.com/archive/p/wmii), which uses
+[`dmenu`](http://tools.suckless.org/dmenu/) to present you all the possible
+executable options in your `$PATH`. I thought, "Wouldn't it be great if `dmenu`
+could present you more descriptive options, yet still spit out the proper
+executable path?" And lo&mdash;a Python script to wrap `dmenu` was born.
+
+This evolved through several forms and languages, and has currently alighted
+upon Go (and, obviously, its current form).
+
+### Overview
+
+`dmenu`'s method of operation is to read a list of options from the standard
+input (one option per line), present these options to the user in a way that
+makes selecting from them with the keyboard efficient, and then printing the
+user's selection on the standard output (followed by a newline). I believe the
+original use case was as a sort of program launcher for
+[`dwm`](http://dwm.suckless.org/) and `wmii`.
+
+The point of this package is to facilitate selecting more human-readable
+options with `dmenu`, but still returning strings that are useful softwarily.
+It provides a framework for associating human-readable options with
+utility-useful options and using `dmenu` to let the user select them.
+
+### Operation
+
+Each option to be passed to `dmenu` is represented by a struct that implements
+the ``dmx.Item`` interface:
+```go
+type Item interface {
+    Key() string
+    MenuLine(int) []byte
+    SortsBefore(Item) bool
+}
+```
+The most important of these methods is `(dmx.Item) MenuLine(int)`, which
+returns the "human-readable" option to be passed to `dmenu`.
+
+This method takes an `int` for aesthetic reasons. To function efficiently with
+`dmenu`, it can be helpful for each option to have a "token" or "key"
+associated with it. For example, here is a sample set of selections that
+might be presented by `dmenu` when running, say, a program launcher:
+```
+writer    LibreOffice Writer
+calc      LibreOffice Calc (Spreadsheet)
+uzbl      uzbl Web Browser
+term      Sakura Terminal Emulator
+wicd      WICD Wireless Client
+arandr    X Output Control and Configuration
+```
+Knowing the length of the longest key that will appear in the menu allows
+`MenuLine()` to space the description text appropriately. This is also where
+the `(dmx.Item) Key()` method comes into play: It should return a "key" so
+that its length can be determined.
+
+A possible implementation that would generate the above menu options might be
+
+```go
+// Choice represents a single option in a program launcher.
+//
+type Choice struct {
+    Token string
+    Description string
+    Value string
+}
+
+// Choice implements dmx.Item
+func (c Choice) Key() string {
+    return c.Token
+}
+
+func (c Choice) MenuLine(width int) []byte {
+    ml_str := fmt.Sprintf("%-*s    %s\n", width, c.Token, c.Description)
+    return []byte(ml_str)
+}
+
+// We don't care about sorting these, but we need to implement dmx.Item.
+func (c Choice) SortsBefore(itm dmx.Item) bool {
+    return true
+}
+```
+
---
+++ dmx.go
@@ -0,0 +1,194 @@
+// dmx.go
+//
+// A package for interacting with suckless tools' dmenu.
+//
+// https://github.com/d2718/dmx
+//
+// by Dan Hill
+// https://github.com/d2718/
+// updated 2017-04-17
+//
+// suckless: http://suckless.org/
+// dmenu:    http://tools.suckless.org/dmenu/
+//
+// This package depends on my dconfig package:
+// https://github.com/d2718/dconfig
+//
+package dmx
+
+import( "bytes"; "fmt"; "os"; "os/exec";
+        "github.com/d2718/dconfig"
+)
+
+var(
+    DmenuPath string = "/usr/local/bin/dmenu"
+    Font string =  "-*-fixed-medium-r-normal--13-*-*-*-*-*-ISO10646-*" // "ProggyCleanTTCE-12"
+    NormalFG   string = "#000"
+    NormalBG   string = "#444"
+    SelectedFG string = "#ddd"
+    SelectedBG string = "#222"
+    CRLF []byte
+    crlfLength int
+)
+
+// Autoconfigure() reads and responds to a configuration file.
+// It first tries all the paths in the supplied slice of strings (which can
+// be nil), then in ~/.config/dmx.conf, then in /usr/share/dmx.conf. It stops
+// and reads from the first one it finds.
+//
+func Autoconfigure(other_cfgs []string) error {
+    dconfig.AddString("dmenu", DmenuPath, dconfig.STRIP)
+    dconfig.AddString("font", Font, dconfig.STRIP)
+    dconfig.AddString("normal_bg", NormalBG, dconfig.STRIP)
+    dconfig.AddString("normal_fg", NormalFG, dconfig.STRIP)
+    dconfig.AddString("selected_bg", SelectedBG, dconfig.STRIP)
+    dconfig.AddString("selected_fg", SelectedFG, dconfig.STRIP)
+    
+    cfg_files := make([]string, 0, 2)
+    for _, fname := range other_cfgs {
+        cfg_files = append(cfg_files, fname)
+    }
+    cfg_files = append(cfg_files, os.ExpandEnv("$HOME/.config/dmx.conf"))
+    cfg_files = append(cfg_files, "/usr/share/dmx.conf")
+    
+    _ = dconfig.Configure(cfg_files, false)
+    
+    DmenuPath, _  = dconfig.GetString("dmenu")
+    Font, _       = dconfig.GetString("Font")
+    NormalFG, _   = dconfig.GetString("normal_fg")
+    NormalBG, _   = dconfig.GetString("normal_bg")
+    SelectedFG, _ = dconfig.GetString("selected_fg")
+    SelectedBG, _ = dconfig.GetString("selected_bg")
+    
+    return nil
+}
+
+// Interface Item represents a single menu item to be passed to dmenu.
+//
+// The intended way for an Item to appear in the menu is:
+//
+// key     more thorough description of item
+//
+// Item.MenuLine() Should return a []byte of a similar format. It takes an int
+// meant to represent the maximum key length among all the Items displayed by
+// dmenu, so that all the descriptions can be lined up properly.
+//
+// Item.Key() should return the key so that DmenuSelect() can calculate the
+// maximum key length of all the items to be displayed.
+//
+// Item.SortsBefore() allows you to make slices of Item sortable by
+// implementing a single method on Item rather than three methods on
+// []Item. If you don't need sortability, just implement something trivial
+// like
+// 
+// func (mi MyItem) SortsBefore(itm dmx.Item) bool { return false }
+//
+type Item interface {
+    Key() string
+    MenuLine(int) []byte
+    SortsBefore(Item) bool
+}
+
+// Type ItemList makes it more convenient to implement sorting for your
+// particular Item implementor. (See the documentation for Item.)
+//
+type ItemList []Item
+
+func (il ItemList) Len() int { return len(il) }
+func (il ItemList) Swap(i, j int) { il[i], il[j] = il[j], il[i] }
+func (il ItemList) Less(i, j int) bool { return il[i].SortsBefore(il[j]) }
+
+func (il ItemList) keyLen() int {
+    kl := 0
+    var nl int
+    for _, itm := range il {
+        nl = len(itm.Key())
+        if nl > kl {
+            kl = nl
+        }
+    }
+    return kl
+}
+
+// If the supplied []byte does not end with a newline, it gets appended and
+// passed back; otherwise, it just gets passed back.
+//
+func ensureReturnminated(bs []byte) []byte {
+    if len(bs) < crlfLength {
+        return append(bs, CRLF...)
+    }
+    
+    if bytes.Equal(bs[len(bs)-crlfLength:], CRLF) {
+        return bs
+    } else {
+        return append(bs, CRLF...)
+    }
+}
+
+// DmenuSelect() runs dmenu externally to allow the user to select one of the
+// Items in the supplied ItemList.
+//
+func DmenuSelect(prompt string, input ItemList) (Item, error) {
+    key_len := input.keyLen()
+    menu_lines := make([][]byte, 0, len(input))
+    for _, itm := range input {
+        menu_lines = append(menu_lines, ensureReturnminated(itm.MenuLine(key_len)))
+    }
+    
+    var dmenu_input  = new(bytes.Buffer)
+    var dmenu_output = new(bytes.Buffer)
+    for _, ml := range menu_lines {
+        dmenu_input.Write(ml)
+    }
+    
+    dcmd := exec.Command(DmenuPath,
+                         "-l", fmt.Sprintf("%d", len(input)),
+                         "-p",  prompt,     "-fn", Font,
+                         "-nb", NormalBG,   "-nf", NormalFG,
+                         "-sb", SelectedBG, "-sf", SelectedFG)
+    dcmd.Stdin  = dmenu_input
+    dcmd.Stdout = dmenu_output
+    err := dcmd.Run()
+    if err != nil {
+        return nil, err
+    }
+    
+    stdout_bytes := dmenu_output.Bytes()
+    for n, ml := range menu_lines {
+        if bytes.Equal(stdout_bytes, ml) {
+            return input[n], nil
+        }
+    }
+    
+    return nil, nil
+}
+
+// Run() is a more primitive interface to dmenu than DmenuSelect().
+// It takes a slice of byte slices (which should NOT be newline-terminated)
+// to pass to dmenu and returns the output of dmenu (which WILL be
+// newline-terminated).
+//
+func Run(prompt string, input [][]byte) ([]byte, error) {
+    
+    dcmd := exec.Command(DmenuPath, "-l", fmt.Sprintf("%d", len(input)),
+                                    "-p", prompt,
+                                    "-fn", Font,
+                                    "-nb", NormalBG, "-nf", NormalFG,
+                                    "-sb", SelectedBG, "-sf", SelectedFG)
+    
+    stdin_slice := make([]byte, 0)
+    for _, bs := range input {
+        stdin_slice = append(stdin_slice, bs...)
+        stdin_slice = append(stdin_slice, CRLF...)
+    }
+    dcmd.Stdin = bytes.NewReader(stdin_slice)
+    var stdout_bytes bytes.Buffer
+    dcmd.Stdout = &stdout_bytes
+    err := dcmd.Run()
+    return stdout_bytes.Bytes(), err
+}
+
+func init() {
+    CRLF = []byte("\n")
+    crlfLength = len(CRLF)
+}
